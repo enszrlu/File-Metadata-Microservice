f90cf037833d70e51db6bca9857b405375dff53b
enszrlu <enes.zrl@gmail.com>
2022-03-17 16:43:17 +0300

    completed version 0.0.1

--- server.js
+++ server.js
@@ -2,17 +2,33 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const bodyParser = require('body-parser')
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+// Create User post API
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send({ name: file.originalname, type: file.mimetype, size: file.size })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
